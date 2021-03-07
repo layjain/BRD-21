1a0a0865f31c6f236fe228632cf9670f7da27f26
layjain <layjain@mit.edu>
2021-03-07 17:33:08 -0500

    Comparison Script

--- utils.py
+++ utils.py
@@ -12,3 +12,16 @@ def date_difference(s1,s2):
     l_date = date(y2, m2, d2)
 
     return (f_date-l_date).days
+
+def calculate_errors(deaths,train_deaths,predicted_deaths,predicted_deaths_base):
+    train_len = len(train_deaths)
+    total_len = len(deaths)
+    sse_baseline = 0 #Sum-Squared-Errors
+    sse_coronavires = 0
+    for i in range(train_len,total_len):
+        baseline_pred = predicted_deaths_base[i]
+        coronavires_predict = predicted_deaths[i]
+        actual_death = deaths[i]
+        sse_baseline += (baseline_pred - actual_death)**2/(total_len-train_len)
+        sse_coronavires += (coronavires_predict - actual_death)**2/(total_len - train_len)
+    return (sse_coronavires**0.5, sse_baseline**0.5)
